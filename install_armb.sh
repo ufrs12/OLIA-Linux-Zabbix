a280c23c399c2ecd36b07238f39d850ce94f16b3
ufrs12 <ufrs12@yandex.ru>
2024-04-29 19:10:04 +0300

    1

--- install_armb.sh
+++ install_armb.sh
@@ -1,3 +1,3 @@
 sudo cp -r OLIA-Linux-Zabbix/zabbix_conf_armb /opt/zabbix_conf_armb
 #APP_ROOT=$(./OLIA-Linux-Zabbix $(./OLIA-Linux-Zabbix $(readlink -fm $0)))
-sudo rm -r "$(dirname "OLIA-Linux-Zabbix/")"
+sudo rm -r "$(dirname "OLIA-Linux-Zabbix")"
